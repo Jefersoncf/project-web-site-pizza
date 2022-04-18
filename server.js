63da57acf1ef1959d024969f8fb423ea88f61cd3
Jeferson Ferreira <fjeferson60@gmail.com>
2022-04-17 22:48:13 -0300

    gitignore

--- server.js
+++ server.js
@@ -3,18 +3,13 @@ const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
 
-
-
 const app = express();
 const port = process.env.PORT || 3000;
-
-
 
 //Templetes engine
 app.use(expressLayout);
 app.use('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
-
 
 app.listen(port, () => {
   console.log('listening on here!')
