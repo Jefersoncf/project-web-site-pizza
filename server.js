382deb6a37a9bc4a258ca8df7b499ecc4f1ef9d3
Jeferson Ferreira <fjeferson60@gmail.com>
2022-04-19 22:48:07 -0300

    criando variaveis css e menu pagina home

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-//Templetes engine
 // app.use(express.urlencoded( { extended: true } ));
 // app.use(expressLayout);
+app.use(express.static('public'));
+
+//Templetes engine
 app.set('views', path.join(__dirname, './resources/views'));
 app.set('view engine', 'ejs');
 
@@ -23,7 +25,3 @@ app.listen(port, () => {
   console.log('listening on here!')
 })
 
-
-// "dev": "cross-env NODE_ENV=development node_modules/webpack/bin/webpack.js --progress --hide-modules --config=node_modules/webpack-mix/setup/webpack.config.js",
-// "watch": "npm run dev --watch",
-// "production": "cross-env NODE_ENV=production node_modules/webpack/bin/webpack.js --progress --hide-modules --config=node_modules/webpack-mix/setup/webpack.config.js"
