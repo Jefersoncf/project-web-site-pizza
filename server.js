1908aa1900d99bd2af93b7f01a2860e349f99479
Jeferson Ferreira <fjeferson60@gmail.com>
2022-05-05 22:51:11 -0300

    Adiocionando Database conexão e models

--- server.js
+++ server.js
@@ -2,9 +2,24 @@ const express = require('express');
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
 const path = require('path');
+const session = require('express-session');
+require('dotenv').config();
+
+const connectDB = require('./app/database/connection');
+
+//Database connection
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+//session config
+app.use(session({
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {maxAge: 1000 * 60 * 60 * 24}
+}))
 
 // app.use(express.urlencoded( { extended: true } ));
 app.use(express.static('public'));
