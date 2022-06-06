0d7bc3ed15ba5de596d7b6d5aa9684e8629d5e75
Jeferson Ferreira <fjeferson60@gmail.com>
2022-06-05 22:30:10 -0300

    iniciando pagina de status do pedido

--- app/http/controllers/customers/orderController.js
+++ app/http/controllers/customers/orderController.js
@@ -31,6 +31,14 @@ function orderController () {
         null, {sort: {'createdAt': -1}}); //sorting in ascending order
         res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
       res.render('customers/orders', { orders: orders, moment: moment });
+    },
+    async show(req, res) {
+      const order = await Order.findById(req.params.id)
+      //Authorize user to
+      if(req.user._id.toString() === order.customerId.toString()){
+        return res.render('customers/singleOrder', {order: order});
+      }
+        return res.redirect('/');
     }
   }
 }
